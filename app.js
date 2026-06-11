16de28f9942bbf60234da32ca60e7ec2c272d776
Mervinnnnn <mervinhuang08@gmail.com>
2026-06-11 11:29:36 +0700

    JSON and POST Request Body. Request param

--- app.js
+++ app.js
@@ -5,31 +5,48 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
     if (req.url === "/" && req.method === "GET") {
         res.write("Hello World from HTTP Node JS!")
-    } else if (req.url === "/hello" && req.method === "GET") {
-        res.write("Hi i am another Text!")
+    } else if (req.url.startsWith("/hello/") && req.method === "GET") {
+
+        const username = req.url.split('/')[2]
+        res.end("Hello " + username)
+
     } else if (req.url === "/world" && req.method === "GET") {
         res.write("Hi i am another world!")
-    } else if(req.url == "/home" && req.method === "GET"){
+    } else if (req.url == "/home" && req.method === "GET") {
         res.write(`
             <h1>Home Page</h1>
             <p>Learning about Node.JS HTTP and Server<p>
             <a HREF="/home">Home</a>
             <a href="/about">About</a>
             
-            `
-        )
-    } else if(req.url == "/about" && req.method === "GET"){
-        fs.readFile('index.html', (err, data)=>{
+            `)
+    } else if (req.url.startsWith('/about/') && req.method === "GET") {
+        const name = req.url.split('/')[2]
+        fs.readFile('index.html', 'utf-8', (err, data) => {
+            const html = data.replace('{{Name}}' , name)
+
+
             res.writeHead(200, {
-                'Content-Type' : 'text/html'
+                'Content-Type': 'text/html'
             })
-            res.write(data)
-            res.end()
+            res.end(html)
         })
         return
+
+    }else if(req.url === '/send' && req.method === "POST"){
+        let body = ""
+        req.on('data', (chunk) =>{
+            body += chunk
+        })
+
+        req.on('end',()=>{
+            const data = JSON.parse(body)
+            console.log(data)
+            res.end("Data send")
+        })
     }else{
-        res.writeHead(404,{
-            'Content-Type' : 'text/html'
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
         })
 
         res.write(`
